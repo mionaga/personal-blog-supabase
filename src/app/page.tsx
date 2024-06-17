e81bf7c0280060cba0575b902bd56d048ef84a27
ItoKona <lupinmine.1217@gmail.com>
2024-06-17 23:35:32 +0900

    add feature/home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
+'use client'
+
 import Image from "next/image";
 import ArticleList from "./components/ArticleList";
-import { getArticles } from "./getters";
+import { getArticles, getCategories } from "./getters";
 import TopAside from "./components/TopAside";
+import { useEffect, useState } from "react";
+import { Category } from "@/types/category";
+import { Article } from "@/types/article";
 
 export default async function Home() {
-  const articles = await getArticles();
+  const [articles, setArticles] = useState<Article[]>([])
+  const [categories, setCategories] = useState<Category[]>([])
+  const [id, setId] = useState<number>()
+
+  useEffect(() => {
+    const fetchTopData = async () => {
+      const articlesData = await getArticles();
+      setArticles(articlesData);
+
+      const categoryData = await getCategories();
+      setCategories(categoryData);
+    }
+    fetchTopData();
+  }, [])
+
+  const handleClick = (id: number) => {
+    const selectedCategory = categories.filter(c => id === c.id)
+    setId(selectedCategory[0].id)
+  }
+
 
   return (
    <div className="container mx-auto md:flex xl:mx-20 xl:gap-x-8 mt-3">
@@ -12,7 +36,10 @@ export default async function Home() {
       <ArticleList articles={articles} />
     </section>
     <aside className="w-full md:w-1/3 flex flex-col items-center px-3 md:pl-6 md:fixed md:top-20 md:right-6">
-      <TopAside />    
+      <TopAside 
+        categories={categories}
+        handleClick={handleClick}  
+      />    
     </aside>
    </div>
   );
